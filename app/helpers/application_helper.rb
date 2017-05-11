9d282565438cc5d0f8a2a29198385e777a1cc084
Surya DaVinci <suryadavinci@gmail.com>
2017-05-11 22:57:03 +0530

    RFR,  Title added dynamically using helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title (title)
+    base = "Ruby On Rails Tutorial"
+    if(title.empty?)
+      base
+    else
+      "#{base} | #{title}"
+    end
+  end      
 end
